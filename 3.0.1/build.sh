49f40d46861e8ff51202b1daf70f0a1548cbfe6f
Javier Burguete <jburguete@eead.csic.es>
2023-01-26 05:16:57 +0100

    Updated build script

--- 3.0.1/build.sh
+++ 3.0.1/build.sh
@@ -1,5 +1,30 @@
+#!/usr/bin/env bash
+if [[ $# != 4 ]]; then
+	echo "The syntax is: ./build.sh A B C D"
+	echo "A: 1 on MPI"
+	echo "B: 1 on native"
+	echo "C: 1 on PGO"
+	echo "D: 1 on strip"
+	exit 1
+fi
+if [[ $1 = 1 ]]; then
+	a1="--with-mpi"
+fi
+if [[ $2 = 1 ]]; then
+	a2="--with-native"
+fi
+if [[ $3 = 1 ]]; then
+	a3="--with-pgo"
+fi
+if [[ $4 = 1 ]]; then
+	a4="strip"
+fi
 aclocal
 autoconf
 automake --add-missing
-./configure --with-pgo
-make
+./configure $a1 $a2 $a3
+if test -x "`command -v gmake`"; then
+	gmake $a4
+else
+	make $a4
+fi
